a60e03d1eb83aad45ec4a73bbe5b106397c7b52d
Pratik Man Shrestha <160830012+Pratik167@users.noreply.github.com>
2026-07-18 00:50:01 +0545

    day19

---
+++ day19.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react";
+
+function Day19(){
+    const [width, setWidth] = useState(window.innerWidth);
+    const[height, setHeight]= useState(window.innerHeight);
+
+    useEffect(()=>{
+
+        window.addEventListener("resize", handleResize);
+        console.log("Event Listener Added");
+
+        return()=>{
+            window.removeEventListener("resize",handleResize);
+            console.log("Event Listener Removed");
+        }
+    },[]);
+    
+    useEffect(()=>{
+        document.title=`Size:${width}x${height}`;
+    },[width,height]);
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);        
+    }
+    return (<>
+    <p>Window Width: {width}px</p>
+    <p>Window Width: {height}px</p>
+    </>);
+}
+
+export default Day19;
